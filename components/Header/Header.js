fc95f3f193ae73ec80cfffdae6a3e81956f76732
shamaz saeed <shamazsaeed332@gmail.com>
2021-08-15 10:53:19 +0500

    search bar added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,25 +1,35 @@
 import Image from "next/image"
-
+import { SearchIcon } from '@heroicons/react/solid'
 const Header = () => {
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 
-       {/* left      */}
+            {/* left      */}
 
-<div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
 
-<Image src="https://links.papareact.com/qd3"
-layout="fill"
-objectFit="contain"
-objectPosition="left"
+                <Image src="https://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
 
-/>
+                />
 
-</div>
+            </div>
 
-<div></div>
+            {/* search bar */}
 
-<div></div>
+            <div className="flex items-center md:border-2
+            py-2
+            md:shadow-sm
+            rounded-full
+            ">
+
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-grey-600 placeholder-gray-400" type="text" placeholder="Start your search" />
+                <SearchIcon className="md:mx-2 h-8 hidden md:inline-flex bg-red-400 rounded-full text-white p-2 cursor-pointer" />
+            </div>
+
+            <div></div>
         </header>
     )
 }
